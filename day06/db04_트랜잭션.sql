c29ccc52e6495722bc5be09fb6999bdb4b0cf8b5
kce0508 <kce0508@gmail.com>
2025-03-05 17:28:15 +0900

    6일차 커밋

---
+++ day06/db04_트랜잭션.sql
@@ -0,0 +1,32 @@
+-- 08 트랜잭션
+-- 테이블 생성
+CREATE TABLE Bank(
+	name VARCHAR(40) PRIMARY KEY,
+    balance	INTEGER
+);
+
+-- 데이터 추가
+INSERT INTO Bank VALUES ('박지성', 100000), ('김연아', 100000);
+
+-- 트랜잭션은 업무 논리적 단위(All or Nothing)
+-- START TRANSACTION;으로 트랜잭션 시작
+-- 성공시 COMMIT; 실패시 ROLLBACK;
+START TRANSACTION;
+-- 박지성 계좌를 읽어온다
+SELECT * FROM Bank WHERE name = '박지성';
+-- 김연아 계좌 읽어온다
+SELECT * FROM Bank WHERE name = '김연아';
+
+-- 박지성 계좌에서 10000원 인출
+UPDATE Bank SET
+	   balance = balance - 10000
+WHERE name = '박지성';
+
+-- 김연아 계좌에서 10000원 인출
+UPDATE Bank SET
+	   balance = balance + 10000
+WHERE name = '김연아';
+
+-- 트랜잭션 종료
+COMMIT;
+ROLLBACK;
